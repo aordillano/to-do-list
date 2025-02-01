b109106c78e2dd2d77a607ef7f94473b03dd9e38
Alieyah May Ordillano <134305793+aordillano@users.noreply.github.com>
2025-01-31 20:34:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const taskText = document.getElementById("add-text");
+    const addBtn = document.getElementById("add-button");
+
+    const addTask = () => {
+        if (taskText.value.length == 0) {
+            alert("No task submitted.");
+        } else {
+            const task = taskText.value; // get task entered by user
+            let div = document.createElement("div"); // create div element
+            div.classList.add("tasks-to-do"); // add tasks class for CSS purposes
+
+            let display = `
+                <input type="checkbox">
+                <label>${task}</label>
+            `; // template literal for html of display text
+
+            div.innerHTML = display; // put display variable as html for div
+            document.getElementById("tasks-to-do").appendChild(div);
+
+            taskText.value = ""; // clear textbox once done
+        }
+    };
+
+    addBtn.addEventListener("click", addTask);
+
+    // when enter is pressed, button is clicked
+    document.addEventListener("keypress", event => {
+        if (event.key === 'Enter') {
+            addBtn.click();
+        }
+    });
+});
