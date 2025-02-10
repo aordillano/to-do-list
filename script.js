d4134eca301d885cc9e2cbd5ec6f245809866569
aordillano <134305793+aordillano@users.noreply.github.com>
2025-02-10 14:09:41 -0500

    delete commented code

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const taskText = document.getElementById("add-text");
     const addBtn = document.getElementById("add-button");
 
-    // const taskList = [];
-
     const addTask = () => {
         if (taskText.value.length == 0) {
             alert("No task submitted.");
@@ -12,20 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
             let div = document.createElement("div"); // create div element
             div.classList.add("tasks-to-do"); // add tasks class for CSS purposes
 
-            // const index = taskList.length;
-            // const newTask = {
-            //     number: taskList.length,
-            //     "task text": task,
-            //     "check id": "check" + String(index),
-            // };
-            // classList.push(newTask);
-
             let display = `
                 <input type="checkbox"> 
                 <label>${task}</label>
-            `; // template literal for html of display text
-            // id="${newTask["check id"]}"
-            // for="${newTask["check id"]}"
+            `;
 
             div.innerHTML = display; // put display variable as html for div
             document.getElementById("tasks-to-do").appendChild(div);
@@ -35,16 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     addBtn.addEventListener("click", addTask);
-    
-    // const labels = document.querySelectorAll("");
-    // for (let i=0; i<taskList.length; i++) {
-    //     const labelForCheck = "check" + String(i);
-        
-    // }
-
-    // const editTask = () => {
-        
-    // };
 
     // when enter is pressed, button is clicked
     document.addEventListener("keypress", event => {
